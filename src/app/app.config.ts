4ad3b9588d397f2b7b399456ef65d34bd0b88a9f
Jacob Staten <jscoding10@protonmail.com>
2025-01-19 13:23:27 -0800

    Added application routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+// import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideCharts(withDefaultRegisterables())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    // provideCharts(withDefaultRegisterables()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AddCurrencyComponent } from './pages/add-currency/add-currency.component';
+import { CryptoInformationComponent } from './pages/crypto-information/crypto-information.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'add-currency', component: AddCurrencyComponent },
+  { path: 'crypto-information/:id', component: CryptoInformationComponent },
+];
